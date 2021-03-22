aea7dd2afebd342bd14ed55adf34660d3e3a8236
Sandesh <sandesh.baliga@compass.com>
2021-03-22 18:06:21 +0530

    Removed all detekt stuff

--- app/src/main/java/com/example/detekt/MainActivity.kt
+++ app/src/main/java/com/example/detekt/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.detekt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.detekt.internal.Foo
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val foo = Foo()
+        foo.toString()
     }
 }
